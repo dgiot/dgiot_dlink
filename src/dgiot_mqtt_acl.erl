580c591960b6653b20f88ce50254a9928a71b677
nizhiguo <nizhiguo@shining3d.com>
2022-01-22 15:27:34 +0800

    %t token校验

--- src/dgiot_mqtt_acl.erl
+++ src/dgiot_mqtt_acl.erl
@@ -20,23 +20,23 @@
 
 %% ACL Callbacks
 -export([
-        check_acl/5
-        , description/0
-       ]).
+  check_acl/5
+  , description/0
+]).
 
-check_acl(ClientInfo = #{ clientid := _Clientid }, PubSub, Topic, _NoMatchAction, _Params) ->
-    _Username = maps:get(username, ClientInfo, undefined),
-    Acls = [],
-    case match(ClientInfo, PubSub, Topic, Acls) of
-        allow ->
-            ok;
+check_acl(ClientInfo, PubSub, Topic, _NoMatchAction, _Params) ->
+  _Username = maps:get(username, ClientInfo, undefined),
+  Acls = [],
+  case match(ClientInfo, PubSub, Topic, Acls) of
+    allow ->
+      ok;
 %%            {stop, allow};
-        deny ->
+    deny ->
 %%            {stop, deny};
-              {stop, allow};
-        _ ->
-            ok
-    end.
+      {stop, allow};
+    _ ->
+      ok
+  end.
 
 description() -> "Acl with Mnesia".
 
@@ -44,32 +44,51 @@ description() -> "Acl with Mnesia".
 %% Internal functions
 %%-------------------------------------------------------------------
 
-match(_ClientInfo,  _PubSub, _Topic, []) ->
-    nomatch;
-match(ClientInfo, PubSub, Topic, [ {_, ACLTopic, Action, Access, _} | Acls]) ->
-    case match_actions(PubSub, Action) andalso match_topic(ClientInfo, Topic, ACLTopic) of
-        true -> Access;
-        false -> match(ClientInfo, PubSub, Topic, Acls)
-    end.
+match(_ClientInfo, _PubSub, _Topic, []) ->
+  nomatch;
+match(ClientInfo, PubSub, Topic, [_ | Acls]) ->
+  match(ClientInfo, PubSub, Topic, Acls);
+
+match(ClientInfo, PubSub, Topic, [{_, ACLTopic, Action, Access, _} | Acls]) ->
+  case match_actions(PubSub, Action) andalso match_topic(ClientInfo, Topic, ACLTopic) of
+    true -> Access;
+    false -> match(ClientInfo, PubSub, Topic, Acls)
+  end.
 
 match_topic(ClientInfo, Topic, ACLTopic) when is_binary(Topic) ->
-    emqx_topic:match(Topic, feed_var(ClientInfo, ACLTopic)).
+  {Acc, Token} = feed_var(ClientInfo, ACLTopic),
+  MatchResult = emqx_topic:match(Topic, Acc),
+  case Token == undefined of
+    true ->
+      MatchResult;
+    _ ->
+      Username = maps:get(username, ClientInfo, undefined),
+      Password = maps:get(password, ClientInfo, undefined),
+      case dgiot_auth:get_session(Token) of
+        #{<<"username">> := Username, <<"password">> := Password} ->
+          MatchResult;
+        _ -> false
+      end
+  end.
 
 match_actions(subscribe, sub) -> true;
 match_actions(publish, pub) -> true;
 match_actions(_, _) -> false.
 
 feed_var(ClientInfo, Pattern) ->
-    feed_var(ClientInfo, emqx_topic:words(Pattern), []).
-feed_var(_ClientInfo, [], Acc) ->
-    emqx_topic:join(lists:reverse(Acc));
-feed_var(ClientInfo = #{clientid := undefined}, [<<"%c">>|Words], Acc) ->
-    feed_var(ClientInfo, Words, [<<"%c">>|Acc]);
-feed_var(ClientInfo = #{clientid := ClientId}, [<<"%c">>|Words], Acc) ->
-    feed_var(ClientInfo, Words, [ClientId |Acc]);
-feed_var(ClientInfo = #{username := undefined}, [<<"%u">>|Words], Acc) ->
-    feed_var(ClientInfo, Words, [<<"%u">>|Acc]);
-feed_var(ClientInfo = #{username := Username}, [<<"%u">>|Words], Acc) ->
-    feed_var(ClientInfo, Words, [Username|Acc]);
-feed_var(ClientInfo, [W|Words], Acc) ->
-    feed_var(ClientInfo, Words, [W|Acc]).
+  feed_var(ClientInfo, emqx_topic:words(Pattern), [], undefined).
+feed_var(_ClientInfo, [], Acc, TokenAcc) ->
+  {emqx_topic:join(lists:reverse(Acc)), TokenAcc};
+feed_var(ClientInfo = #{clientid := undefined}, [<<"%c">> | Words], Acc, TokenAcc) ->
+  feed_var(ClientInfo, Words, [<<"%c">> | Acc], TokenAcc);
+feed_var(ClientInfo = #{clientid := ClientId}, [<<"%c">> | Words], Acc, TokenAcc) ->
+  feed_var(ClientInfo, Words, [ClientId | Acc], TokenAcc);
+feed_var(ClientInfo = #{username := undefined}, [<<"%u">> | Words], Acc, TokenAcc) ->
+  feed_var(ClientInfo, Words, [<<"%u">> | Acc], TokenAcc);
+feed_var(ClientInfo = #{username := Username}, [<<"%u">> | Words], Acc, TokenAcc) ->
+  feed_var(ClientInfo, Words, [Username | Acc], TokenAcc);
+feed_var(ClientInfo = #{password := Token}, [<<"%t">> | Words], Acc, TokenAcc) when TokenAcc == undefined ->
+  feed_var(ClientInfo, Words, Acc, Token);
+
+feed_var(ClientInfo, [W | Words], Acc, TokenAcc) ->
+  feed_var(ClientInfo, Words, [W | Acc], TokenAcc).
